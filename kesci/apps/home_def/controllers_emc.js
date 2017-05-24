15451ee6e464bbab5ab5b964a55d9295fb612b5e
WangLei <nihiue@gmail.com>
2017-05-24 20:12:40 +0800

    clean repo

--- kesci/apps/home_def/controllers_emc.js
+++
@@ -1,9 +0,0 @@
-//Home_Default controllers.js
-
-var myAppModule = angular.module('myApp', []);
-
-myAppModule.controller('headerCtr',
-function($scope) {
-	$scope.isReg=false;
-	$scope.showPinDiv=false;
-});
